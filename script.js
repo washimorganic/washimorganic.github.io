16822da190f7b58d9921048863c653456ecb0e42
washimorganic <washim.raja89@gmail.com>
2025-04-03 23:34:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showContact() {
+    let contactInfo = document.getElementById("contact-info");
+    if (contactInfo.style.display === "none") {
+        contactInfo.style.display = "block";
+    } else {
+        contactInfo.style.display = "none";
+    }
+}
